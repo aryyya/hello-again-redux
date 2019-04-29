f8516ce48db4b8fe4510e40a6c1ec736ca141792
aryyya <aryyya.xyz@gmail.com>
2019-04-28 20:37:24 -0700

    Integrate Redux without any React helper libraries!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
 import React, { useState } from 'react'
 import './App.css'
+import { createStore } from 'redux'
+
+const defaultTodos = {
+  todos: [
+    {
+      text: 'Wake up.',
+      isComplete: true
+    },
+    {
+      text: 'Make breakfast.',
+      isComplete: false
+    },
+    {
+      text: 'Head to class.',
+      isComplete: false
+    }
+  ]
+}
+
+const todos = (state = defaultTodos, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          action.todo
+        ]
+      }
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: [
+          ...state.todos.map(todo => {
+            if (todo.text === action.todo.text) {
+              todo.isComplete = !todo.isComplete
+            }
+            return todo
+          })
+        ]
+      }
+    default:
+      return state
+  }
+}
+
+export const store = createStore(todos)
 
 const App = () => {
   return (
@@ -22,7 +69,15 @@ const TodoInputForm = () => {
 
   const addTodo = (event) => {
     event.preventDefault()
-    console.log(`Adding todo: ${todoText}`)
+
+    store.dispatch({
+      type: 'ADD_TODO',
+      todo: {
+        text: todoText,
+        isComplete: false
+      }
+    })
+
     setTodoText('')
   }
 
@@ -43,11 +98,32 @@ const TodoInputForm = () => {
 }
 
 const TodoList = () => {
+  const { todos } = store.getState()
+
+  const toggleTodo = todo => {
+    store.dispatch({
+      type: 'TOGGLE_TODO',
+      todo
+    })
+  }
+
   return (
     <ul className="list-group rounded-0">
-      <li className="list-group-item rounded-0">Get apples.</li>
-      <li className="list-group-item rounded-0">Get bananas.</li>
-      <li className="list-group-item rounded-0">Get pears.</li>
+      {todos.map(todo => {
+        const style = {
+          userSelect: 'none',
+          textDecoration: todo.isComplete ? 'line-through' : 'none'
+        }
+        return (
+          <li
+            className="list-group-item rounded-0"
+            style={style}
+            onClick={() => toggleTodo(todo)}
+            key={todo.text}>
+            {todo.text}
+          </li>
+        )
+      })}
     </ul>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App, { store } from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root')
+const renderApp = () => ReactDOM.render(<App />, root)
+store.subscribe(renderApp)
+renderApp()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
