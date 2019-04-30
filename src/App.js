d7b7c95f14f5fce2aa7c3a6230969d25b910e38e
aryyya <aryyya.xyz@gmail.com>
2019-04-30 14:32:51 -0700

    Add filter counts.

--- src/App.js
+++ src/App.js
@@ -285,6 +285,7 @@ const TodoList = () => {
 }
 
 const VisibilityButtons = () => {
+  const { todos } = store.getState()
 
   const setVisibility = filter => {
     store.dispatch({
@@ -292,6 +293,12 @@ const VisibilityButtons = () => {
       filter
     })
   }
+
+  const count = todos.reduce((count, todo) => ({
+    ...count,
+    complete: todo.isComplete ? count.complete + 1 : count.complete,
+    incomplete: !todo.isComplete ? count.incomplete + 1 : count.incomplete
+  }), { complete: 0, incomplete: 0 })
 
   const buttonStyle = {
     fontSize: '14px'
@@ -303,21 +310,21 @@ const VisibilityButtons = () => {
         style={buttonStyle}
         type="button"
         onClick={() => setVisibility('SHOW_ALL')}>
-        All
+        All ({todos.length})
       </button>
       <button
         className="btn btn-warning col rounded-0"
         style={buttonStyle}
         type="button"
         onClick={() => setVisibility('SHOW_COMPLETE')}>
-        Complete
+        Complete ({count.complete})
       </button>
       <button
         className="btn btn-danger col rounded-0"
         style={buttonStyle}
         type="button"
         onClick={() => setVisibility('SHOW_INCOMPLETE')}>
-        Incomplete
+        Incomplete ({count.incomplete})
       </button>
     </div>
   )
